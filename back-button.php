577a9f0fddd46d51cff62ce1a6c0cb896a0b8282
VagishVela <web@vagish.com>
2014-04-24 12:08:23 +0100

    Cleaned up code further.

--- back-button.php
+++ back-button.php
@@ -20,19 +20,22 @@
  * GitHub Plugin URI: https://github.com/VagishVela/
  */
 
+// Queue Stylesheet
+function vv_back_button() {
+	wp_register_style( 'vv_back_button_css', plugins_url('inc/css/back-button.css', __FILE__), false, '1.0.0', true );
+	
+	wp_enqueue_style( 'vv_back_button_css' );
+}
+add_action( 'wp_enqueue_scripts', 'vv_back_button' );
+
+// Find referrer then create back button if it is the same server.
 add_filter('wp_footer', 'back_button_prev_page');
 function back_button_prev_page() {
-	if (empty($_SERVER['HTTP_REFERER'])) {
+	if (empty($_SERVER['HTTP_REFERER']) || is_front_page() ) {
 		return;
 	} else {
 		  $backbuttonurl = htmlspecialchars($_SERVER['HTTP_REFERER']);
+		  echo "<div class=\"back-button\"> <p class=\"back-button-txt\"><a href='$backbuttonurl' onclick=\"if (document.referrer.indexOf(window.location.host) !== -1) { history.go(-1); return false; } else { window.location.href = ' . get_site_url(); . '; }\">&#171;</a></p></div>";
 	}
-
-	if ( is_front_page() ) {
-		return;
-	} else if (isset($backbuttonurl)) {
-  		echo "<div class=\"back-button\"> <p class=\"back-button-txt\"><a href='$backbuttonurl' onclick=\"if (document.referrer.indexOf(window.location.host) !== -1) { history.go(-1); return false; } else { window.location.href = ' . get_site_url(); . '; }\">&#171;</a></p></div>";
-	}
-
 };
 ?>
